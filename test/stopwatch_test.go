ef45ae45393444f8f30c4703a253356c30b45f2f
sadlil <sadlil.rhythm@gmail.com>
2016-01-13 23:19:07 +0600

    other package imports.

---
+++ test/stopwatch_test.go
@@ -0,0 +1,27 @@
+package test
+
+import (
+	"fmt"
+	"github.com/sadlil/stopwatch"
+	"github.com/sadlil/stopwatch/timeunit"
+	"testing"
+	"time"
+)
+
+func TestStopWatch(t *testing.T) {
+	first := stopwatch.New()
+	first.Start()
+	fmt.Println(first.ElapsedNanos())
+	fmt.Println(first.ElapsedString())
+	time.Sleep(time.Second)
+	fmt.Println(first.ElapsedNanos())
+	fmt.Println(first.Elapsed(timeunit.DAYS),
+		first.Elapsed(timeunit.HOURS),
+		first.Elapsed(timeunit.MINUTES),
+		first.Elapsed(timeunit.SECONDS),
+		first.Elapsed(timeunit.MILLISECONDS),
+		first.Elapsed(timeunit.MICROSECONDS),
+		first.Elapsed(timeunit.NANOSECONDS),
+	)
+	fmt.Println(first.ElapsedString())
+}
